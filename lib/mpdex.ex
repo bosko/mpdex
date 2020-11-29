80468e3b8c8b638a76983b0542c4ba70499c371b
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2020-11-29 18:13:54 +0100

    feat: mpdex is now GenServer and will receive/emit MPD state changes

--- lib/mpdex.ex
+++ lib/mpdex.ex
@@ -1,91 +1,141 @@
 defmodule Mpdex do
-  @moduledoc """
-  Mpdex is small library for communicating with Music Player Daemon.
-  """
+  use GenServer
+
+  def start_link(host: host, port: port, name: name) do
+    GenServer.start_link(__MODULE__, %{host: host, port: port}, name: name)
+  end
+
+  def start_link(host: host, port: port) do
+    GenServer.start_link(__MODULE__, %{host: host, port: port}, name: __MODULE__)
+  end
 
   @doc """
-  Configures host and port to which Mpdex will connect.
-
-  If not called or called with empty list Mpdex will work on default
-  MPD settings - localhost on port 6600
-
-  It accepts following options:
+  Client accepts following options:
 
     * `:host` - IP address on which MPD is
     * `:port` - port number on which MPD listens
 
+  and stores them in the application environment so socket client can
+  later fetch those settings in order to communicate with MPD.
+
   ## Examples
 
-    Mpdex.configure(host: "192.168.0.5", port: 6600)
+    GenServer.start_link(Mpdex.MpdClient, %{host: "127.0.0.1", port: 6600})
 
   """
-  def configure(options \\ []) do
-    Application.put_env(:mpdex, :host, Keyword.get(options, :host, "localhost"))
-    Application.put_env(:mpdex, :port, Keyword.get(options, :port, 6600))
+  @impl true
+  def init(%{host: host, port: port}) do
+    Application.put_env(:mpdex, :host, host)
+    Application.put_env(:mpdex, :port, port)
+
+    case connect(host, port) do
+      {:ok, socket, version} ->
+        :inet.setopts(socket, active: true)
+        :gen_tcp.send(socket, "idle\n")
+
+        {:ok,
+         %{
+           status: :connected,
+           host: host,
+           port: port,
+           socket: socket,
+           mpd_version: version,
+           clients: []
+         }}
+
+      {:error, error} ->
+        {:ok, %{status: :disconnected, host: host, port: port, socket: nil, error: error}}
+    end
   end
 
   @doc """
   Gets the list of play lists.
   """
-  defdelegate list, to: Mpdex.Playlists
+  def list(mpd) do
+    GenServer.call(mpd, :playlists)
+  end
 
   @doc """
   Gets the content of the list.
   """
-  defdelegate get(list_name), to: Mpdex.Playlists
+  def get(mpd, list_name) do
+    GenServer.call(mpd, {:list, list_name})
+  end
 
   @doc """
   Loads the list content into the queue.
   """
-  defdelegate load(list_name), to: Mpdex.Playlists
+  def load(mpd, list_name) do
+    GenServer.call(mpd, {:load, list_name})
+  end
 
   @doc """
   Adds URI the list. List will be created if it does not exist.
   """
-  defdelegate add_to_list(list_name, uri), to: Mpdex.Playlists
+  def add_to_list(mpd, list_name, uri) do
+    GenServer.call(mpd, {:add_to_list, list_name, uri})
+  end
 
   @doc """
   Clears the play list.
   """
-  defdelegate clear_list(list_name), to: Mpdex.Playlists, as: :clear
+  def clear_list(mpd, list_name) do
+    GenServer.call(mpd, {:clear_list, list_name})
+  end
 
   @doc """
   Deletes song at position from the play list.
   """
-  defdelegate delete_song_at(list_name, position), to: Mpdex.Playlists
+  def delete_song_at(mpd, list_name, position) do
+    GenServer.call(mpd, {:delete_at, list_name, position})
+  end
 
   @doc """
   Moves song from the position from to the position to with the play
   list.
   """
-  defdelegate move_song(list_name, from, to), to: Mpdex.Playlists
+  def move_song(mpd, list_name, from, to) do
+    GenServer.call(mpd, {:move_song, list_name, from, to})
+  end
 
   @doc """
   Save the current queue to the new list.
   """
-  defdelegate save_queue_to_list(list_name), to: Mpdex.Playlists
+  def save_queue_to_list(mpd, list_name) do
+    GenServer.call(mpd, {:save_queue_to_list, list_name})
+  end
 
   @doc """
   Renames the list
   """
-  defdelegate rename(list_name, new_name), to: Mpdex.Playlists
+  def rename(mpd, list_name, new_name) do
+    GenServer.call(mpd, {:rename_list, list_name, new_name})
+  end
 
   @doc """
   Deletes list
   """
-  defdelegate delete(list_name), to: Mpdex.Playlists
+  def delete(mpd, list_name) do
+    GenServer.call(mpd, {:delete_list, list_name})
+  end
 
   @doc "Displays content of the queue"
-  defdelegate queue, to: Mpdex.Queue, as: :list
+  def queue(mpd) do
+    GenServer.call(mpd, :queue)
+  end
 
   @doc """
   Adds URI to the queue. If URI is directory it will be added
   recursively. Otherwise single file or URL is added.
   """
-  defdelegate add_to_queue(uri), to: Mpdex.Queue, as: :add
+  def add_to_queue(mpd, uri) do
+    GenServer.call(mpd, {:add_to_queue, uri})
+  end
 
   @doc "Clears the queue"
-  defdelegate clear, to: Mpdex.Queue
+  def clear(mpd) do
+    GenServer.call(mpd, :clear_queue)
+  end
 
   @doc """
   Deletes song or range of songs from the queue.
@@ -107,7 +157,9 @@ defmodule Mpdex do
     {:ok, "OK\n}
 
   """
-  defdelegate remove_song(options), to: Mpdex.Queue, as: :delete
+  def remove_song(mpd, options) do
+    GenServer.call(mpd, {:remove_from_queue, options})
+  end
 
   @doc """
   Moves song or range of songs to the given position.
@@ -130,7 +182,9 @@ defmodule Mpdex do
     {:ok, "OK\n}
 
   """
-  defdelegate move_song(options), to: Mpdex.Queue, as: :move
+  def move_song(mpd, options) do
+    GenServer.call(mpd, {:move_song, options})
+  end
 
   @doc """
   Shuffles queue.
@@ -142,28 +196,40 @@ defmodule Mpdex do
 
   Without options shuffles entire queue.
   """
-  defdelegate shuffle_queue(options), to: Mpdex.Queue, as: :shuffle
-
-  defdelegate idle(subsystem), to: Mpdex.Status
+  def shuffle_queue(mpd, options) do
+    GenServer.call(mpd, {:shuffle, options})
+  end
 
   @doc "Returns statistics."
-  defdelegate statistics, to: Mpdex.Status
+  def statistics(mpd) do
+    GenServer.call(mpd, :statistics)
+  end
 
   @doc "Returns current playback status."
-  @spec status() :: map()
-  defdelegate status(), to: Mpdex.Status
+  @spec status(any()) :: map()
+  def status(mpd) do
+    GenServer.call(mpd, :status)
+  end
 
   @doc "Sets crossfading between songs."
-  defdelegate crossfade(seconds), to: Mpdex.Playback
+  def crossfade(mpd, seconds) do
+    GenServer.call(mpd, {:crossface, seconds})
+  end
 
   @doc "Plays next song in the queue."
-  defdelegate next, to: Mpdex.Playback
+  def next(mpd) do
+    GenServer.call(mpd, :next)
+  end
 
   @doc "Pauses playback."
-  defdelegate pause, to: Mpdex.Playback
+  def pause(mpd) do
+    GenServer.call(mpd, :pause)
+  end
 
   @doc "Resumes playback."
-  defdelegate resume, to: Mpdex.Playback
+  def resume(mpd) do
+    GenServer.call(mpd, :resume)
+  end
 
   @doc """
   Begins playing the playlist at song position or with song ID.
@@ -171,44 +237,265 @@ defmodule Mpdex do
   iex> Mpdex.play(:position, 2)
   iex> Mpdex.play(:id, 23)
   """
-  defdelegate play(what, val), to: Mpdex.Playback
+  def play(mpd, what, val) do
+    GenServer.call(mpd, {:play, what, val})
+  end
 
   @doc "Plays previous song in the queue."
-  defdelegate previous, to: Mpdex.Playback
+  def previous(mpd) do
+    GenServer.call(mpd, :previous)
+  end
 
   @doc "Turns random off."
-  defdelegate random_off, to: Mpdex.Playback
+  def random_off(mpd) do
+    GenServer.call(mpd, :random_off)
+  end
 
   @doc "Turns random on."
-  defdelegate random_on, to: Mpdex.Playback
+  def random_on(mpd) do
+    GenServer.call(mpd, :random_on)
+  end
 
   @doc "Turns repeat off."
-  defdelegate repeat_off, to: Mpdex.Playback
+  def repeat_off(mpd) do
+    GenServer.call(mpd, :repeat_off)
+  end
 
   @doc "Turns repeat on."
-  defdelegate repeat_on, to: Mpdex.Playback
+  def repeat_on(mpd) do
+    GenServer.call(mpd, :repeat_on)
+  end
 
   @doc """
   Seeks current song to the position time (in seconds; fractions
   allowed).
   """
-  defdelegate seek(time), to: Mpdex.Playback
+  def seek(mpd, time) do
+    GenServer.call(mpd, {:seek, time})
+  end
 
   @doc """
   Seeks forward current song to the position time (in seconds;
   fractions allowed) relative to current position.
   """
-  defdelegate forward(time), to: Mpdex.Playback
+  def forward(mpd, time) do
+    GenServer.call(mpd, {:forward, time})
+  end
 
   @doc """
   Seeks backward current song to the position time (in seconds;
   fractions allowed) relative to current position.
   """
-  defdelegate backward(time), to: Mpdex.Playback
+  def backward(mpd, time) do
+    GenServer.call(mpd, {:backward, time})
+  end
 
   @doc "Stops playing"
-  defdelegate stop, to: Mpdex.Playback
+  def stop(mpd) do
+    GenServer.call(mpd, :stop)
+  end
 
   @doc "Sets playback volume (from 0 to 100)"
-  defdelegate volume(vol), to: Mpdex.Playback
+  def volume(mpd, vol) do
+    GenServer.call(mpd, {:volume, vol})
+  end
+
+  @impl true
+  def handle_call(mpd_cmd, _from, state) do
+    res =
+      case mpd_cmd do
+        :playlists ->
+          Mpdex.Playlists.list()
+
+        {:list, list_name} ->
+          Mpdex.Playlists.get(list_name)
+
+        {:load, list_name} ->
+          Mpdex.Playlists.load(list_name)
+
+        {:add_to_list, list, uri} ->
+          Mpdex.Playlists.add_to_list(list, uri)
+
+        {:clear_list, list_name} ->
+          Mpdex.Playlists.clear(list_name)
+
+        {:delete_at, list_name, position} ->
+          Mpdex.Playlists.delete_song_at(list_name, position)
+
+        {:move_song, list_name, from, to} ->
+          Mpdex.Playlists.move_song(list_name, from, to)
+
+        {:save_queue_to_list, list_name} ->
+          Mpdex.Playlists.save_queue_to_list(list_name)
+
+        {:rename_list, list_name, new_name} ->
+          Mpdex.Playlists.rename(list_name, new_name)
+
+        {:delete_list, list_name} ->
+          Mpdex.Playlists.delete(list_name)
+
+        :queue ->
+          Mpdex.Queue.list()
+
+        {:add_to_queue, uri} ->
+          Mpdex.Queue.add(uri)
+
+        :clear_queue ->
+          Mpdex.Queue.clear()
+
+        {:remove_from_queue, options} ->
+          Mpdex.Queue.delete(options)
+
+        {:move_song, options} ->
+          Mpdex.Queue.move(options)
+
+        {:shuffle, options} ->
+          Mpdex.Queue.shuffle(options)
+
+        :statistics ->
+          Mpdex.Status.statistics()
+
+        :status ->
+          Mpdex.Status.status()
+
+        {:crossfade, seconds} ->
+          Mpdex.Playback.crossfade(seconds)
+
+        :next ->
+          Mpdex.Playback.next()
+
+        :pause ->
+          Mpdex.Playback.pause()
+
+        :resume ->
+          Mpdex.Playback.resume()
+
+        {:play, what, val} ->
+          Mpdex.Playback.play(what, val)
+
+        :previous ->
+          Mpdex.Playback.previous()
+
+        :random_off ->
+          Mpdex.Playback.random_off()
+
+        :random_on ->
+          Mpdex.Playback.random_on()
+
+        :repeat_off ->
+          Mpdex.Playback.repeat_off()
+
+        :repeat_on ->
+          Mpdex.Playback.repeat_on()
+
+        {:seek, time} ->
+          Mpdex.Playback.seek(time)
+
+        {:forward, time} ->
+          Mpdex.Playback.forward(time)
+
+        {:backward, time} ->
+          Mpdex.Playback.backward(time)
+
+        :stop ->
+          Mpdex.Playback.stop()
+
+        {:volume, volume} ->
+          Mpdex.Playback.volume(volume)
+      end
+
+    {:reply, res, state}
+  end
+
+  @impl true
+  def handle_info({:tcp, socket, data}, state) do
+    :binary.list_to_bin(data)
+    |> String.split("\n")
+    |> Enum.reduce([], fn chg_info, acc ->
+      case chg_info do
+        "changed: mixer" ->
+          if false == Enum.member?(acc, :status) do
+            [:status | acc]
+          else
+            acc
+          end
+
+        "changed: player" ->
+          if false == Enum.member?(acc, :status) do
+            [:status | acc]
+          else
+            acc
+          end
+
+        "changed: playlist" ->
+          if false == Enum.member?(acc, :queue) do
+            [:queue | acc]
+          else
+            acc
+          end
+
+        "OK" ->
+          acc
+
+        _other ->
+          acc
+      end
+    end)
+    |> Enum.each(fn what ->
+      case what do
+        :status ->
+          status = Mpdex.Status.status()
+          Enum.each(state.clients, fn client -> send(client, {:status, status}) end)
+
+        :queue ->
+          queue = Mpdex.Queue.list()
+          Enum.each(state.clients, fn client -> send(client, {:queue, queue}) end)
+      end
+    end)
+
+    :gen_tcp.send(socket, "idle\n")
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:tcp_closed, _socket}, state) do
+    state =
+      case connect(state.host, state.port) do
+        {:ok, socket, _version} ->
+          :inet.setopts(socket, active: true)
+          :gen_tcp.send(socket, "idle\n")
+
+          Map.put(state, :socket, socket)
+
+        {:error, error} ->
+          Map.merge(state, %{
+            status: :disconnected,
+            socket: nil,
+            error: error
+          })
+      end
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:tcp_error, _socket, _reason}, state) do
+    {:noreply, state}
+  end
+
+  defp connect(host, port) when is_binary(host) and is_integer(port) do
+    with {:ok, socket} <- :gen_tcp.connect(String.to_charlist(host), port, active: false),
+         {:ok, resp} <- :gen_tcp.recv(socket, 0) do
+      case to_string(resp) do
+        <<"OK MPD ", version::binary>> ->
+          {:ok, socket, String.trim(version)}
+
+        _ ->
+          {:error, nil}
+      end
+    else
+      err -> {:error, err}
+    end
+  end
 end
